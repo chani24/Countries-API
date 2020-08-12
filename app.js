a51b1ed62c9b9261603c2b249832a9c6fcb3607e
Ani Chidera <achidera20@gmail.com>
2020-08-12 09:07:10 +0100

    fixed cors errors and parsed multi name countries better

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ let listOfCountriesJson = JSON.parse(fs.readFileSync('countries.json', 'utf-8'))
 const countryNames = listOfCountriesJson.map(country => country.name);
 
 
-
+app.use((req,res,next)=>{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers',
+  'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods',
+  'GET, POST, PATCH, DELETE, OPTIONS');
+  next();
+});
 
 app.listen(process.env.PORT);
 
@@ -19,6 +26,7 @@ app.get('/countries/list', function (req, res) {
 
   app.get('/countries/details/:country', function (req, res) {
     const country = req.params.country;
+    const parsedCountries = country.split('-').join(' ');
 
     const countryDetails = countriesJson.find(countries => countries.name == country);
 
